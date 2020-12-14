78b53fe76c5a61c8526bbf1af7de8ac3beed6559
Vinisha Nagarajan <vinisha@Vinishas-MacBook-Pro.local>
2020-12-14 17:04:26 -0500

    setup - initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>To-Do List</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
